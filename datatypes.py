9f4cd16cd663109a207ffd15edf4137e4757e8d0
24331a05f0 <24331a05f0@mvgrce.edu.in>
2025-12-19 13:15:43 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,8 @@
+student_name = "Teja"
+student_age = 19
+student_grade = 'A'
+student_percentage = 92.5
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+print("Student Grade:", student_grade)
+print("Student Percentage:", student_percentage)
