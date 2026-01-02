8ce9f260d3e03cb637683eb2847c0f0cd3a6236e
24331a05f0 <24331a05f0@mvgrce.edu.in>
2026-01-02 14:08:43 +0530

    Add files via upload

---
+++ 1D_array.py
@@ -0,0 +1,6 @@
+import numpy as np
+lst =[2,4,8]
+a=np.array(lst)
+print("orignal array:",a)
+print("slicing of array 1 to 2:",a[1:2])
+print("index of element",a[2],a[1])
---
+++ 2d_array.py
@@ -0,0 +1,4 @@
+import numpy as np
+lst = [[3,5,7],[9,1,5]]
+a= np.array(lst)
+print("2D array:\n",a)
