2258beeb2bbefe9492d243690ecc2aa84898d685
24331a05f0 <24331a05f0@mvgrce.edu.in>
2026-02-13 12:11:45 +0530

    Add files via upload

---
+++ lager.py
@@ -0,0 +1,5 @@
+marks =[98,97,87,99,95]
+print("Minimum marks are:",min(marks))
+print("Maximum marks are:",max(marks))
+
+
--- string.py
+++ string.py
@@ -1,7 +1,4 @@
-a = "Hello"
-b = "World"
-
-c = a + " " + b
-
-print(c)
-
+str = input("Enter a string: ")
+ltr = input("Enter a letter: ")
+count = str.count(ltr)
+print(f"The letter {ltr} repeated {count} times")
